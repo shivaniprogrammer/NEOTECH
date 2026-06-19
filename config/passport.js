e77bf17bd2a407debd1853a9b5ef0c9f0389744b
Shivani Sivakumar <shivanisivakumar0903@gmail.com>
2026-06-19 17:56:19 +0530

    fix passport callbackURL to absolute URL

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/auth/google/callback'
+  callbackURL: 'https://neotech-gg5m.onrender.com/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if user already exists
